9ef556376b4693b9bad838b1676f1a3818cb550e
csd79 <denes.cselovszky@gmail.com>
2024-06-27 13:24:00 +0200

    cref v0.01

---
+++ cref.v01.lisp
@@ -0,0 +1,451 @@
+(defparameter *illetmeny*
+  '(
+    (:havi-ill-gyakornok "Havi illetmény - Gyakornok"
+     (:puetv
+      (:par "98."
+       (:bek "(2)")))
+     (:puetv-vhr
+      (:par "88."
+       (:bek "(1)"))))
+
+    (:havi-ill-ped1-kutato "Havi illetmény - Pedagógus I. - Kutatótanár"
+     (:puetv
+      (:par "98."
+       (:bek "(2)")))
+     (:puetv-vhr
+      (:par "88/A."
+       (:bek "(1)"))))
+
+    (:mesterfok-illnov "Mesterfokozatért járó illetménynövekedés"
+     (:puetv-vhr
+      (:par "95/A."
+       (:bek "(1)"))))
+
+    (:egyes-tantrgy-illnov "Egyes tantárgyak után járó illetménynövekedés"
+     (:puetv
+      (:par "95/A."
+       (:bek "(2)")
+       (:bek "(4)"))))
+
+    (:eselyteremt-illresz-terulet "Területi alapon járó esélyteremtési illetményrész"
+     (:puetv
+      (:par "98."
+       (:bek "(5)"
+        (:pont "a)"))))
+     (:puetv-vhr
+      (:par "88."
+       (:bek "(3)")
+       (:bek "(4)")
+       (:bek "(6)"))
+      (:par "131."
+       (:bek "(4)"))))
+
+    (:eselyteremt-illresz-feladat "Feladatalapon járó esélyteremtési illetményrész"
+     (:puetv
+      (:par "98."
+       (:bek "(5)"
+        (:pont "b)"))))
+     (:puetv-vhr
+      (:par "88."
+       (:bek "(4)-(6)"))
+      (:par "131."
+       (:bek "(4)"))))
+
+    (:gyogyped-potl "Gyógypedagógiai pótlék"
+     (:puetv
+      (:par "103."
+       (:bek "(1)"))
+      (:mell "3."
+       (:pont "B)")))
+     (:puetv-vhr
+      (:par "90."
+       (:bek "(3)")
+       (:bek "(4a)"))))
+
+    (:diabetesz-ellat "Diabétesz ellátási pótlék"
+     (:puetv
+      (:par "103."
+       (:bek "(1)"))
+      (:mell "3."
+       (:pont "B)")))
+     (:puetv-vhr
+      (:par "90."
+       (:bek "(4)")))
+     (:koznev-2011-cxc
+      (:par "62."
+       (:bek "(1b)"))))
+
+    (:nemzetisegi-potl "Nemzetiségi pótlék"
+     (:puetv
+      (:par "103."
+       (:bek "(1)"))
+      (:mell "3."
+       (:pont "B)")))
+     (:puetv-vhr
+      (:par "90."
+       (:bek "(2)")
+       (:bek "(4a)"))))
+
+    (:aj-thgond-prg "Arany János Tehetséggondozó Program pótléka"
+     (:puetv-vhr
+      (:par "90."
+       (:bek "(7)")))
+     (:emmi-20-2012-viii31
+      (:par "176."
+       (:bek "(8)"))))
+
+    (:ofo-megbiz "Osztályfõnöki megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(3)"
+        (:pont "b)"))))
+     (:puetv-vhr
+      (:par "90."
+       (:bek "(6)")
+       (:bek "(6a)"))))
+
+    (:koll-csopvez "Kollégiumi csoportvezetõi megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(3)"
+        (:pont "b)"))))
+     (:puetv-vhr
+      (:par "90."
+       (:bek "(6)")
+       (:bek "(6a)"))))
+
+    (:tanszakvez "Tanszakvezetõi megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(3)"
+        (:pont "b)"))))
+     (:puetv-vhr
+      (:par "90."
+       (:bek "(6)")
+       (:bek "(6a)"))))
+
+    (:mkkoz-vez "Munkaközösség-vezetõi megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(3)"
+        (:pont "c)"))))
+     (:puetv-vhr
+      (:par "90."
+       (:bek "(5)")
+       (:bek "(6)")
+       (:bek "(6a)"))))
+
+    (:cimz-ig "Címzetes igazgatói megbízási díj"
+     (:puetv
+      (:par "73."
+       (:bek "(12)"))))
+    
+    (:cimz-foig "Címzetes fõigazgatói megbízási díj"
+     (:puetv
+      (:par "73."
+       (:bek "(12)"))))
+    
+    (:ig "Igazgatói megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(2)"))))
+    
+    (:foig "Fõigazgatói megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(2)"))))
+    
+    (:ig-h "Igazgató-helyettesi megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(2)"
+        (:pont "a)")))))
+    
+    (:foig-h "Fõigazgató-helyettesi megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(2)"
+        (:pont "a)")))))
+    
+    (:tagint-ig "Tagintézmény-igazgatói megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(4)"))))
+    
+    (:integys-vez "Intézményegység-vezetõi megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(4)"))))
+    
+    (:tagint-ig-h "Tagintézményigazgató-helyettesi megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(5)"))))
+    
+    (:integys-vez-h "Intézményegységvezetõ-helyettesi megbízási díj"
+     (:puetv
+      (:par "102."
+       (:bek "(1)")
+       (:bek "(5)"))))
+  
+    (:pednoks-havi-ill-gyakornok "Havi illetmény - Gyakornok"
+     (:puetv
+      (:par "96."
+       (:bek "(1)")
+       (:bek "(2)"
+        (:pont "a)")))
+      (:par "98."
+       (:bek "(2)")))
+     (:puetv-vhr
+      (:par "88."
+       (:bek "(1)"))))
+    
+    (:havi-ill-ped1-2 "Havi illetmény - Pedagógus I. - II."
+     (:puetv
+      (:par "96."
+       (:bek "(1)")
+       (:bek "(2)"
+        (:pont "a)")))
+      (:par "98."
+       (:bek "(2)")))
+     (:puetv-vhr
+      (:par "88/A."
+       (:bek "(1)"))))
+    
+    (:pednoks-noks-eselyteremt-illresz "Esélyteremtési illetményrész"
+     (:puetv
+      (:par "99."
+       (:bek "(2)"))
+      (:par "98."
+       (:bek "(5)"
+        (:pont "a)"))))
+     (:puetv-vhr
+      (:par "88."
+       (:bek "(3)")
+       (:bek "(4)")
+       (:bek "(6)"))
+      (:par "131."
+       (:bek "(4)"))))
+    
+    (:pednoks-noks-aj-thgond-prg "Arany János Tehetséggondozó Program pótléka"
+     (:puetv-vhr
+      (:par "90."
+       (:bek "(7)")))
+     (:emmi-20-2012-viii31
+      (:par "176."
+       (:bek "(8)"
+        (:megj "(A jogszabályi rendelkezések a pedagógusokra vonatkoznak.)")))))
+  
+    (:noks-havi-ill "Havi illetmény"
+     (:puetv
+      (:par "96."
+       (:bek "(2)"
+        (:pont "b)")))
+      (:par "99."
+       (:bek "(1)")))
+     (:puetv-vhr
+      (:par "88."
+       (:bek "(7)"))
+      (:par "95."
+       (:bek "(1)"))))
+    ))
+
+
+(defparameter *pedagogus*
+  '(:havi-ill-gyakornok
+    :havi-ill-ped1-kutato
+    :mesterfok-illnov
+    :egyes-tantrgy-illnov
+    :eselyteremt-illresz-terulet
+    :eselyteremt-illresz-feladat
+    :gyogyped-potl
+    :diabetesz-ellat
+    :nemzetisegi-potl
+    :aj-thgond-prg
+    :ofo-megbiz
+    :koll-csopvez
+    :tanszakvez
+    :mkkoz-vez
+    :cimz-ig
+    :cimz-foig
+    :ig
+    :foig
+    :ig-h
+    :foig-h
+    :tagint-ig
+    :integys-vez
+    :tagint-ig-h
+    :integys-vez-h))
+
+
+(defparameter *pednoks*
+  '(:pednoks-havi-ill-gyakornok
+    :havi-ill-ped1-2
+    :mesterfok-illnov
+    :pednoks-noks-eselyteremt-illresz
+    :gyogyped-potl
+    :diabetesz-ellat
+    :pednoks-noks-aj-thgond-prg))
+
+
+(defparameter *noks*
+  '(:noks-havi-ill
+    :pednoks-noks-eselyteremt-illresz
+    :gyogyped-potl
+    :diabetesz-ellat
+    :pednoks-noks-aj-thgond-prg))
+
+
+(defun fee-by-key (key list)
+  (let ((pos (position key list :key #'first)))
+    (when pos
+      (let ((result (nth pos list)))
+        (values (cddr result)
+                (first result)
+                (second result))))))
+
+
+(defun collect-fees (keys list)
+  (let ((result '()))
+    (dolist (key keys)
+      (push (fee-by-key key list)
+            result))
+    (apply #'append
+           (nreverse
+            (remove nil result)))))
+
+
+(defparameter s '(:foig :ig-h :integys-vez :tanszakvez :mkkoz-vez))
+(defparameter g '(:foig :ig-h :integys-vez :tanszakvez :mkkoz-vez :nemzetisegi-potl :havi-ill-ped1-2))
+
+
+
+
+
+(defun level0 (results)
+  (let ((categories (remove-duplicates
+                     (mapcar #'first results))))
+    (apply #'append
+           (mapcar #'(lambda (key)
+                       (remove-if-not #'(lambda (list)
+                                          (eq (first list) key))
+                                      results ))))))
+
+
+
+#|
+
+
+
+((:PUETV (:PAR "102." (:BEK "(1)") (:BEK "(2)")))
+ (:PUETV (:PAR "102." (:BEK "(1)") (:BEK "(2)" (:PONT "a)"))))
+ (:PUETV (:PAR "102." (:BEK "(1)") (:BEK "(4)")))
+ (:PUETV (:PAR "102." (:BEK "(1)") (:BEK "(3)" (:PONT "b)"))))
+ (:PUETV-VHR (:PAR "90." (:BEK "(6)") (:BEK "(6a)")))
+ (:PUETV (:PAR "102." (:BEK "(1)") (:BEK "(3)" (:PONT "c)"))))
+ (:PUETV-VHR (:PAR "90." (:BEK "(5)") (:BEK "(6)") (:BEK "(6a)")))
+ (:PUETV (:PAR "103." (:BEK "(1)")) (:MELL "3." (:PONT "B)")))
+ (:PUETV-VHR (:PAR "90." (:BEK "(2)") (:BEK "(4a)")))
+ (:PUETV (:PAR "96." (:BEK "(1)") (:BEK "(2)" (:PONT "a)"))) (:PAR "98." (:BEK "(2)")))
+ (:PUETV-VHR (:PAR "88/A." (:BEK "(1)"))))
+
+
+---------------------------------------------------------
+
+
+((:PUETV (:PAR "102." (:BEK "(1)") (:BEK "(2)")))
+ (:PUETV (:PAR "102." (:BEK "(1)") (:BEK "(2)" (:PONT "a)"))))
+ (:PUETV (:PAR "102." (:BEK "(1)") (:BEK "(4)")))
+ (:PUETV (:PAR "102." (:BEK "(1)") (:BEK "(3)" (:PONT "b)"))))
+ (:PUETV (:PAR "102." (:BEK "(1)") (:BEK "(3)" (:PONT "c)"))))
+ (:PUETV (:PAR "103." (:BEK "(1)")) (:MELL "3." (:PONT "B)")))
+ (:PUETV (:PAR "96." (:BEK "(1)") (:BEK "(2)" (:PONT "a)"))) (:PAR "98." (:BEK "(2)"))))
+
+
+((:PUETV-VHR (:PAR "90." (:BEK "(6)") (:BEK "(6a)")))
+ (:PUETV-VHR (:PAR "90." (:BEK "(5)") (:BEK "(6)") (:BEK "(6a)")))
+ (:PUETV-VHR (:PAR "90." (:BEK "(2)") (:BEK "(4a)")))
+ (:PUETV-VHR (:PAR "88/A." (:BEK "(1)"))))
+
+
+---------------------------------------------------------
+
+:puetv
+((:PAR "102." (:BEK "(1)")
+              (:BEK "(2)"))
+ (:PAR "102." (:BEK "(1)")
+              (:BEK "(2)" (:PONT "a)")))
+ (:PAR "102." (:BEK "(1)")
+              (:BEK "(4)"))
+ (:PAR "102." (:BEK "(1)")
+              (:BEK "(3)" (:PONT "b)")))
+ (:PAR "102." (:BEK "(1)")
+              (:BEK "(3)" (:PONT "c)")))
+ (:PAR "103." (:BEK "(1)"))
+ (:MELL "3." (:PONT "B)"))
+ (:PAR "96." (:BEK "(1)")
+             (:BEK "(2)" (:PONT "a)")))
+ (:PAR "98." (:BEK "(2)")))
+
+
+:puetv-vhr
+((:PAR "90." (:BEK "(6)")
+             (:BEK "(6a)"))
+ (:PAR "90." (:BEK "(5)")
+             (:BEK "(6)")
+             (:BEK "(6a)"))
+ (:PAR "90." (:BEK "(2)")
+             (:BEK "(4a)"))
+ (:PAR "88/A." (:BEK "(1)")))
+
+
+--------------------------------------------------
+
+:puetv
+:par
+ ("102." (:BEK "(1)")
+        (:BEK "(2)"))
+ ("102." (:BEK "(1)")
+         (:BEK "(2)" (:PONT "a)")))
+ ("102." (:BEK "(1)")
+         (:BEK "(4)"))
+ ("102." (:BEK "(1)")
+         (:BEK "(3)" (:PONT "b)")))
+ ("102." (:BEK "(1)")
+         (:BEK "(3)" (:PONT "c)")))
+ ("103." (:BEK "(1)"))
+ ("96." (:BEK "(1)")
+        (:BEK "(2)" (:PONT "a)")))
+ ("98." (:BEK "(2)")))
+
+
+:puetv
+:mell
+(("3." (:PONT "B)")))
+
+
+
+:puetv-vhr
+:par
+(("90." (:BEK "(6)")
+        (:BEK "(6a)"))
+ ("90." (:BEK "(5)")
+        (:BEK "(6)")
+        (:BEK "(6a)"))
+ ("90." (:BEK "(2)")
+        (:BEK "(4a)"))
+ ("88/A." (:BEK "(1)")))
+
+
+|#
